a3cf9321ff260a39145db584a811dbff2fd26f7b
devendar <devendar097@gmail.com>
2023-05-18 23:45:23 +0530

    chages and new file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Pokemonn from './components/Pokemonn';
 import './App.css';
 
 function App() {
+
+  const [pokes, setPokes] = useState([]);
+  const [loadPoke, setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const getAllPokemons = async () => {
+    const res = await fetch (loadPoke)
+    const data = await res.json()
+    setLoadPoke(data.next)
+
+    function createPokemonObject(result) {
+      result.forEach(async (pokemon) => {
+        const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json();
+        setPokes(currentList => [...currentList, data])
+      })
+    }
+   createPokemonObject(data.result)
+   await console.log(pokes)
+  }
+
+  useEffect (() => {
+    getAllPokemons()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>Kingdom of Pokemons.</h1>
+     <div>
+      <div>
+        {pokes.map((pokemons,index) =>
+          <Pokemonn 
+            id = {pokemon.id}
+            name = {pokemon.name}
+            image = {pokemon.sprites.other.dream_world.front_default}
+            type = {pokemon.type[0].type.name}
+            key = {index}
+            height = {pokemon.height}
+            weight = {pokemon.weight}
+            stat1 = {pokemon.stats[0].stat.name}
+            stat2 = {pokemon.stats[1].stat.name}
+            stat3 = {pokemon.stats[2].stat.name}
+            stat4 = {pokemon.stats[3].stat.name}
+            stat5 = {pokemon.stats[4].stat.name}
+            stat6 = {pokemon.stats[5].stat.name}
+
+            bs1 = {pokemon.stats[0].base_stat}
+            bs2 = {pokemon.stats[1].base_stat}
+            bs3 = {pokemon.stats[2].base_stat}
+            bs4 = {pokemon.stats[3].base_stat}
+            bs5 = {pokemon.stats[4].base_stat}
+            bs6 = {pokemon.stats[5].base_stat}
+
+          />
+        )}
+      </div>
+       <button onClick={() => getAllPokemons()}>more pokemonss</button>
+     </div>
+    
     </div>
   );
 }
---
+++ src/components/Pokemonn.js
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+import Description from './Description';
+
+const Pokemonn = ({id,name,image,type,height,weight,stat1,stat2,stat3,stat4,stat5,stat6,bs1,bs2,bs3,bs4,bs5,bs6}) => {
+
+    const style = `thumb-container ${type}`
+    const [show , setShow] = useState(false);
+
+  return (
+    <div>
+      <div>
+        <small>#0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div>
+      <h3>{name.toUpperCase()}</h3>
+      <small>Type : {type}</small>
+      <button onClick={() => setShow(!show)}>
+        {show===true? "know less..." : "know more..."}
+      </button>
+      {show===true?
+      <Description 
+      weightpok={weight} 
+      heightpok={height} 
+
+      pokstat1={stat1}
+      pokstat2={stat2}
+      pokstat3={stat3}
+      pokstat4={stat4}
+      pokstat5={stat5}
+      pokstat6={stat6}
+
+      posbs1={bs1}
+      posbs2={bs2}
+      posbs3={bs3}
+      posbs4={bs4}
+      posbs5={bs5}
+      posbs6={bs6}
+      /> : <></> }
+
+      </div>
+     </div>
+  )
+}
+
+export default Pokemonn
